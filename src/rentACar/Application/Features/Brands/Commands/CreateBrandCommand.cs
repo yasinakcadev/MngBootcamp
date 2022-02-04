84f6d7cbef570dff7ee480ecc744881aa7366117
yasinakca <yasinakcadev@gmail.com>
2022-02-04 11:45:17 +0300

    Email server added

--- src/rentACar/Application/Features/Brands/Commands/CreateBrandCommand.cs
+++ src/rentACar/Application/Features/Brands/Commands/CreateBrandCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Mailing;
 using Domain.Entities;
 using MediatR;
 
@@ -15,13 +16,15 @@ public class CreateBrandCommand : IRequest<Brand>
         IBrandRepository _brandRepository;
         IMapper _mapper;
         BrandBusinessRules _brandBusinessRules;
+        IMailService _mailService;
 
         public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper,
-            BrandBusinessRules brandBusinessRules)
+            BrandBusinessRules brandBusinessRules, IMailService mailService)
         {
             _brandRepository = brandRepository;
             _mapper = mapper;
             _brandBusinessRules = brandBusinessRules;
+            _mailService = mailService;
         }
 
         public async Task<Brand> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
@@ -31,6 +34,13 @@ public class CreateBrandCommand : IRequest<Brand>
             var mappedBrand = _mapper.Map<Brand>(request);
 
             var createdBrand = await _brandRepository.AddAsync(mappedBrand);
+            var mail = new Mail { 
+                ToFullName = "system admins",
+                ToEmail = "admins@mng.com.tr",
+                Subject = "New Brand Added",
+                HtmlBody = "Hey, check the system!"
+            };
+            _mailService.sendMail(mail);
             return createdBrand;
         }
     }
