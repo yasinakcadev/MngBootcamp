1d77083c7aaecda0e3982dfc9aee6514fc8616f8
SevtapE <sevtaperbek@outlook.com>
2022-02-20 19:21:23 +0300

    home component- getting info

--- frontend/rentACar/src/app/core/components/city/city.component.ts
+++ frontend/rentACar/src/app/core/components/city/city.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { CityListModel } from '../../models/listModels/cityListModel';
 import { ListResponseModel } from '../../models/listResponseModel';
 import { CityService } from '../../services/city.service';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-city',
@@ -11,9 +12,12 @@ import { CityService } from '../../services/city.service';
 export class CityComponent implements OnInit {
   cities: ListResponseModel<CityListModel> = { items: [] };
   selectedCity: any;
+  @Output() giveSelectedCityIdEvent = new EventEmitter<any>();
+
   constructor(private cityService: CityService) {}
 
   ngOnInit(): void {
+
     this.getCities();
   }
 
@@ -21,10 +25,18 @@ export class CityComponent implements OnInit {
     this.cityService.getCities(0, 100).subscribe((data) => {
       this.cities = data;
       console.log('selectedcity', this.selectedCity);
+
+
     });
   }
 
   getSelectedCity(selectedCity: any) {
-    console.log('burdan geldim', selectedCity);
+    console.log('burdan geldim', this.selectedCity);
+  }
+
+  giveSelectedCityId() {
+    this.giveSelectedCityIdEvent.emit(this.selectedCity);
+    console.log(this.selectedCity)
+
   }
 }
